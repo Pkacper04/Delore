fcbe721fad64ae1930229cd3523ec83a8a59180a
Pkacper04 <61903308+Pkacper04@users.noreply.github.com>
2022-01-04 12:33:58 +0100

    Pause Done
    1. Pause Done
    2. Setting menu (Waiting)
    3. Exit menu (Waiting)
    4. Save system(Waiting)
        - Save Player Position(Can do)
        - Save Player Decisions(Waiting)
        - Save Player Rotation(Can do)*
        - Save Player Items(Waiting)
        - Save Music Settings(Waiting)

--- Assets/PauseController.cs
+++ Assets/PauseController.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool GamePaused = false;
+    [SerializeField] private GameObject PausePanel;
+
+
+    private void Start()
     {
-        
+        PausePanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //sdsdsdsd
+        if(Input.GetKeyDown(KeyCode.E)) // do zmiany na esc po wsyzstkich testach
+        {
+            GamePaused = !GamePaused;
+            PauseGame();
+        }
     }
+
+    private void PauseGame()
+    {
+        if(GamePaused)
+        {
+            Time.timeScale = 0f;
+            PausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            PausePanel.SetActive(false);
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        GamePaused = false;
+        PauseGame();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Wyjscie z gry");
+        //SceneManager.LoadScene("Menu");
+        //Dokoñczyæ gdy bêdzie gotowa scena menu
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings on");
+        //Dodaæ uruchamianie ustawieñ w przysz³oœci
+    }
+
+    public void Save()
+    {
+        Debug.Log("Saved Game");
+        //Zapisywanie po kliknieciu stanu gry te¿ w przysz³oœci
+    }
+
 }

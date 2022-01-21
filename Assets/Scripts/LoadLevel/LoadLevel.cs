5b3ffccb699069e3bbfd05ca5f5df52743b89eb8
Pkacper04 <61903308+Pkacper04@users.noreply.github.com>
2022-01-21 19:22:27 +0100

    LoadingScreenDone

--- Assets/Scripts/LoadLevel/LoadLevel.cs
+++ Assets/Scripts/LoadLevel/LoadLevel.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 public class LoadLevel : MonoBehaviour
 {
     private bool loaded = false;
-    private Image loadingBar;
+    [SerializeField] private Image loadingBar;
+    [SerializeField] private TMP_Text loadingText;
     void Start()
     {
-        loadingBar = GetComponent<Image>();
         loadingBar.fillAmount = 0;
-        
+        loadingText.enabled = false;
         StartCoroutine(LoadScene());
     }
 
@@ -24,13 +25,16 @@ public class LoadLevel : MonoBehaviour
         while (!loaded)
         {
             loadingBar.fillAmount = Mathf.Clamp01(operation.progress/.9f);
-            Debug.Log(loadingBar.fillAmount);
+
             if(operation.progress == .9f)
                 loaded = true;
             yield return null;
         }
         while(loaded)
         {
+            loadingBar.color = new Color(255, 255, 255, 0);
+            loadingText.enabled = true;
+
             if (Input.anyKeyDown)
                 operation.allowSceneActivation = true;
             yield return null;
